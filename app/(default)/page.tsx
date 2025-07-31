f02623bc0aa1b6139df03cc8472ab700149383a5
it.sainathgoud <it.sainathgoud@gmail.com>
2025-07-30 20:23:13 -0700

    Initial commit

---
+++ app/(default)/page.tsx
@@ -0,0 +1,64 @@
+export const metadata = {
+  title: "Mindtris | Accelerate Business Transformation",
+  description:
+    "Mindtris is a global technology consulting and digital solutions firm that partners with organizations across industries to reimagine business models, accelerate innovation, and drive sustainable growth through digital transformation. Serving both public and private sector clients, we bring deep domain expertise and cutting-edge technology capabilities to deliver differentiated customer experiences and impactful business outcomes in an increasingly connected world.",
+  keywords: [
+    "Mindtris",
+    "digital transformation",
+    "technology excellence",
+    "talent mobilization",
+    "customer experiences",
+    "healthcare transformation",
+    "business growth solutions",
+    "engineering",
+    "security & surveillance",
+  ],
+  authors: [{ name: "Mindtris Inc.", url: "https://mindtris.com" }],
+    openGraph: {
+    title: "Mindtris | Accelerate Business Transformation",
+    description: "Mindtris is a global technology consulting and digital solutions firm that partners with organizations across industries to reimagine business models, accelerate innovation, and drive sustainable growth through digital transformation. Serving both public and private sector clients, we bring deep domain expertise and cutting-edge technology capabilities to deliver differentiated customer experiences and impactful business outcomes in an increasingly connected world.",
+    url: "https://mindtris.com",
+    siteName: "Mindtris",
+    images: [
+      {
+        url: "https://mindtris.com/assets/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Mindtris IT Consulting & IT Services",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mindtris | Accelerate Business Transformation",
+    description: "Mindtris is a global technology consulting and digital solutions firm that partners with organizations across industries to reimagine business models, accelerate innovation, and drive sustainable growth through digital transformation. Serving both public and private sector clients, we bring deep domain expertise and cutting-edge technology capabilities to deliver differentiated customer experiences and impactful business outcomes in an increasingly connected world.",
+    images: ["https://mindtris.com/assets/twitter-image.jpg"],
+  },
+};
+
+
+// MINDTRIS landing page starts here..  
+
+import PageIllustration from "@/components/page-illustration";
+import Hero from "@/components/hero-home";
+import Workflows from "@/components/workflows";
+import Features from "@/components/features";
+import Pricing from "@/components/pricing-home";
+import SplitCarousel from "@/components/split-carousel";
+import Cta from "@/components/cta";
+
+export default function Home() {
+  return (
+    <>
+      <PageIllustration />
+      <Hero />
+      <Workflows />
+      <Features />
+      <SplitCarousel />
+      <Pricing />
+      <Cta />
+    </>
+  );
+}
---
+++ components/ui/header.tsx
@@ -0,0 +1,299 @@
+"use client";
+
+import Link from "next/link";
+import Logo from "./logo";
+import Dropdown from "@/components/dropdown";
+import MobileMenu from "./mobile-menu";
+
+export default function Header() {
+  return (
+    <header className="z-30 mt-2 w-full md:mt-5">
+      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+        <div className="relative flex h-14 items-center justify-between gap-3 rounded-2xl bg-gray-900/90 px-3 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,var(--color-gray-800),var(--color-gray-700),var(--color-gray-800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] after:absolute after:inset-0 after:-z-10 after:backdrop-blur-xs">
+          {/* Site branding */}
+          <div className="flex flex-1 items-center">
+            <Logo />
+          </div>
+
+          {/* Desktop navigation */}
+          <nav className="hidden md:flex md:grow">
+            {/* Desktop menu links */}
+            <ul className="flex grow flex-wrap items-center justify-center gap-4 text-sm lg:gap-8">
+              {/* <li>
+                <Link
+                  href="/pricing"
+                  className="flex items-center px-2 py-1 text-gray-200 transition hover:text-indigo-500 lg:px-3"
+                >
+                  Pricing
+                </Link>
+              </li> */}
+              {/* Services section start here.. */}
+              <Dropdown title="Services">
+                {/* 2nd level: hover */}
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Digital Transformation
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Technology Excellence
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Customer Experiences
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Talent Mobilization
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Healthcare Transformation
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Business Growth Solutions
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Engineering
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Security & Surveillance
+                  </Link>
+                </li>
+              </Dropdown>
+
+              {/* Industries section start here.. */}
+              <Dropdown title="Industries">
+                {/* 2nd level: hover */}
+                <li>
+                  <Link
+                    href="/newsletter"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Banking & Financial Services
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/contact"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Retail & Consumer Goods
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Healthcare & Life Sciences
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Public Sector
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Telecommunications
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Healthcare & Life Sciences
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Energy & Utilities
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Travel, Transportation & Hospitality
+                  </Link>
+                </li>
+              </Dropdown>
+              <li>
+                <Link
+                  href="/404"
+                  className="flex items-center px-2 py-1 text-gray-200 transition hover:text-indigo-500 lg:px-3"
+                >
+                  Products
+                </Link>
+              </li>
+              {/* 1st level: hover */}
+              <Dropdown title="Resources">
+                {/* 2nd level: hover */}
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Blogs
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Success Stories
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Client Testimonials
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    White Papers
+                  </Link>
+                </li>
+              </Dropdown>
+              {/* <li>
+                <Link
+                  href="/blog"
+                  className="flex items-center px-2 py-1 text-gray-200 transition hover:text-indigo-500 lg:px-3"
+                >
+                  Blog
+                </Link>
+              </li> */}
+              {/* <li>
+                <Link
+                  href="/help/frequently-asked-questions"
+                  className="flex items-center px-2 py-1 text-gray-200 transition hover:text-indigo-500 lg:px-3"
+                >
+                  Help Centre
+                </Link>
+              </li> */}
+              {/* 1st level: hover About Us*/}
+              <Dropdown title="About">
+                {/* 2nd level: hover */}
+                <li>
+                  <Link
+                    href="/about"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    About Us
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/#0"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Careers
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/#0"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Sustainability
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/#0"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Partners
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/404"
+                    className="flex rounded-lg px-2 py-1.5 text-sm text-white hover:text-indigo-500"
+                  >
+                    Diversity & Inclusion
+                  </Link>
+                </li>
+              </Dropdown>
+            </ul>
+          </nav>
+
+          {/* Desktop sign in links */}
+          <ul className="flex flex-1 items-center justify-end gap-3">
+            <li>
+              <Link
+                href="/signin"
+                className="btn-sm relative bg-linear-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] py-[5px] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,var(--color-gray-800),var(--color-gray-700),var(--color-gray-800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%]"
+              >
+                Sign In
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/contact"
+                className="btn-sm bg-linear-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] py-[5px] text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%]"
+              >
+                Schedule a Call
+              </Link>
+            </li>
+          </ul>
+
+          <MobileMenu />
+        </div>
+      </div>
+    </header>
+  );
+}
