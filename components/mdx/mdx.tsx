ef0071263337c5b79b889db37de9985e14beb46b
it.sainathgoud <it.sainathgoud@gmail.com>
2025-08-12 10:37:28 -0700

    Add custom heading components for MDX rendering

--- components/mdx/mdx.tsx
+++ components/mdx/mdx.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-// import { MDXRemote } from "next-mdx-remote/rsc";
-import { MDXRemote } from "next-mdx-remote";
+import { MDXRemote } from "next-mdx-remote/rsc";
 import PostLink from "./link";
 import PostImage from "./image";
 import rehypePrettyCode from "rehype-pretty-code";
